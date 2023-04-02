d5b8b473f2e138157048a92f3638014c0cb375a4
Jcortes99 <jcortesro99@gmail.com>
2023-04-02 17:45:24 -0500

    controlles con path

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get('/')
+
+movies = [
+    {
+        "id": 1,
+        "nombre": "mugen train",
+        "Animador": "Funemi animation"
+    },
+    {
+        "id": 2,
+        "nombre": "mugen train",
+        "Animador": "Funemi animation"
+    }
+]
+
+@app.get('/', tags=['home'])
 def message():
-    return "hello world!"
+    return HTMLResponse('<h1>Hello world</h1>')
+
+
+@app.get('/movies', tags=['movies'])
+def get_movies():
+    return movies
+
+@app.get('/movies/{id}')
+def get_movie(id: int):
+    for each_movie in movies:
+        if each_movie["id"] == id:
+            return each_movie["nombre"]
+    return []
